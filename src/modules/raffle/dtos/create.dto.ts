878dee8b0376d272f47f87285844653be2df60e0
Erick M. L. Pacheco <ericklopes02@gmail.com>
2022-10-31 13:08:34 -0300

    feat: allow empty description

--- src/modules/raffle/dtos/create.dto.ts
+++ src/modules/raffle/dtos/create.dto.ts
@@ -9,7 +9,7 @@ export default class RaffleCreateDto {
   constructor(
     public readonly ownerId: string,
     public readonly title: string,
-    public readonly description: string,
+    public description: string,
     public readonly prize: string,
     public readonly remindMe: boolean = false,
     public readonly status: RaffleStatus = RaffleStatus.CREATED,
@@ -27,11 +27,11 @@ export default class RaffleCreateDto {
   static from(body: Partial<RaffleCreateDto>) {
     if (!body.ownerId) throw new MissingFieldException('ownerId');
     if (!body.title) throw new MissingFieldException('title');
-    if (!body.description) throw new MissingFieldException('description');
     if (!body.prize) throw new MissingFieldException('prize');
     if (!body.price) throw new MissingFieldException('price');
     if (!body.fundingTarget) throw new MissingFieldException('fundingTarget');
     if (!body.optionsQty) throw new MissingFieldException('optionsQty');
+    if (!body.description) body.description = '';
     if (body.optionsQty) body.optionsQty = parseInt(body.optionsQty as unknown as string, 10);
 
     return new RaffleCreateDto(
